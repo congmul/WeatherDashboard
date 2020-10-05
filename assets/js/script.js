a1d123ac11f16e113c3bb0f66fc99e883a788d8a
Jehyun <congmul@hotmail.com>
2020-10-05 01:56:54 -0700

    Creating City Button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
-$("#cityName").text("Test" + Date());
-$("#currentDate").text("TestDate");
+var cities = ["Seattle", "Seoul"];
+var searchBtnEl = $("#searchBtn");
+var citiesBtnEl = $("#citiesBtn");
 
-$("#temperature").text("testTemp");
+searchBtnEl.on("click", function () {
+    var cityName = $("#searchInput").val().trim();
+    var cityNameReset = "";
+    for (let i = 0; i < cityName.length; i++) {
+        if (i === 0) {
+            cityNameReset += cityName[i].toUpperCase();
+        } else {
+            cityNameReset += cityName[i].toLowerCase();
+        }
+    }
+    cities.push(cityNameReset);
+    createButton(cities);
+})
+
+function createButton(arrCities) {
+    citiesBtnEl.empty();
+
+    arrCities.forEach(function (data) {
+        var buttonEl = $("<button>");
+        buttonEl.addClass("btn cities");
+        buttonEl.attr("city-name", data);
+        buttonEl.text(data);
+        citiesBtnEl.prepend(buttonEl);
+    })
+}
+
+createButton(cities);
